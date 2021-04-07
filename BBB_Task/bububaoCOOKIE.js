a1ffa775ae33e365fc20c5d95a83c81de71a9ebc
Soul012 <67261074+Soul012@users.noreply.github.com>
2021-04-07 19:29:16 +0800

    Update bububaoCOOKIE.js

--- BBB_Task/bububaoCOOKIE.js
+++ BBB_Task/bububaoCOOKIE.js
@@ -1,7 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let bububaotokenVal = `CA335518CF44212DF47165744534594G1617794161
-                      `
+let bububaotokenVal = `CA335518CF44212DF47165744534594G1617794161`
 
 
 
